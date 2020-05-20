9cf4240aa1c7fdae2dc3227232d8084fcdbd7a3f
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-05-20 23:35:38 +0900

    메인 이미지 컴포넌트 제작

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const API_KEY = 'acbf7e172f282ad305191889d699c899';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,42 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { FaCode } from 'react-icons/fa';
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
-    return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setMovies([response.results]);
+        setMainMovieImage(response.results[0]);
+      });
+  }, []);
+  return (
+    <div style={{ width: '100%', margin: '0' }}>
+      {/* Main Image */}
+      {MainMovieImage && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+          title={MainMovieImage.original_title}
+          text={MainMovieImage.overview}
+        />
+      )}
+
+      <div style={{ width: '80%', margin: '1rem auto' }}>
+        <h2>Movies by latest</h2>
+        <hr />
+
+        {/* Movie Grid Cards */}
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
