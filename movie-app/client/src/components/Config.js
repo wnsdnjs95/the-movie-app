200e6787157e39f8daa8e6a4591f4549eed73c8d
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-05-20 22:40:35 +0900

    the movie app

--- movie-app/client/src/components/Config.js
+++
@@ -1,7 +0,0 @@
-//SERVER ROUTES
-export const USER_SERVER = "/api/users";
-
-export const API_URL = "https://api.themoviedb.org/3/";
-export const API_KEY = "acbf7e172f282ad305191889d699c899";
-
-export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://junwon:wnsdnjs12!@@project1-g7jai.mongodb.net/test?retryWrites=true&w=majority',
+};
