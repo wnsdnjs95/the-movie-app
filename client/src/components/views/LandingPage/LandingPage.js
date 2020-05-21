2fc5875feb4fc11209aefe5fced32e0be28c3d13
wnsdnjs95 <43410943+wnsdnjs95@users.noreply.github.com>
2020-05-21 15:46:07 +0900

    Revert "Update LandingPage.js"
    This reverts commit bfd93b9b1d1fbf4dd7a0d5e457445703a232d376.

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -36,6 +36,12 @@ function LandingPage() {
       .then((result) => result.json())
 
       .then((result) => {
+        // console.log(result)
+
+        // console.log('Movies',...Movies)
+
+        // console.log('result',...result.results)
+
         setMovies([...Movies, ...result.results]);
 
         setMainMovieImage(MainMovieImage || result.results[0]);
